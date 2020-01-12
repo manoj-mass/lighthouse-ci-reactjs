6df7d3829937cda210de254a16d5de171c324dec
Maskorala, Manoj <Manoj.Masakorala@pearson.com>
2020-01-12 22:12:59 +0530

    removed unwanted tags

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <input type="text"></input>
-        <input type="text"></input>
-        <input type="text"></input>
-        <img src="https://www.digimarkcentral.com/wp-content/uploads/2016/11/How-Does-a-CDN-Work.png"></img>
         <a
           className="App-link"
           href="https://reactjs.org"
