e17ac0f4099027e46e822f350ed2290477e88b8c
Maskorala, Manoj <Manoj.Masakorala@pearson.com>
2020-01-13 09:31:11 +0530

    input feild added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type="text"></input>
         <a
           className="App-link"
           href="https://reactjs.org"
